b475effca0a9e663bf0ebad156201176d5f12630
yas2carvalho <yasmim.c.silva16@aluno.senai.br>
2025-01-29 08:28:12 -0300

    Initial commit

---
+++ aritmeticos.js
@@ -0,0 +1,25 @@
+//console.log("Olá, Mundo!")
+
+
+let nome = "Yas"
+var nomes = "Yas"
+
+//CTRL + ; -> COMENTARIOS
+console.log("Meu nome é", nome)
+
+let idade = 16
+console.log("Minha idade é", idade, "anos")
+
+let a = 3 
+let b = 2
+
+let soma = a + b
+let sub = a - b
+let mult = a * b
+let div = a / b
+
+
+console.log(soma)
+console.log(sub)
+console.log(mult)
+console.log(div)
